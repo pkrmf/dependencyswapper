9c09d30c8d254e9e9fcc8f238c999093d35d2c0b
Marc Terns <tenxurz@gmail.com>
2017-09-08 17:02:12 -0500

    added extra check for already existing local paths

--- lib/dependencyswapper/cli.rb
+++ lib/dependencyswapper/cli.rb
@@ -4,31 +4,22 @@
  module Dependency
    class CLI < Thor
    	desc "test FrameworkName", "Pulls the FrameworkName as source(not editable)"
+   	option :exclude_repo
   	def test(name)
     	Dependency::DependencySwapper.new({
 			:dependency_name => name
+			#:excluded_directory => "#{options[:exclude_repo]}"
 		}).run
   	end
 
   	desc "development FrameworkName", "Pulls the FrameworkName as development pod(editable source)"
+  	option :exclude_repo
   	def dev(name)
+  		puts "#{options[:exclude_repo]}"
     	Dependency::DependencySwapper.new({
 			:dependency_name => name
+			#:excluded_directory => "#{options[:exclude_repo]}"
 		}).dev
   	end
-
-  	desc "setup", "Initial setup for depswapper"
-  	def setup
-  		puts "Make sure to add your dependency mappings in the ~/.depswapper/depmapper.json file!"
-  		`if [ ! -d ~/.depswapper ]; then
-  			mkdir -p ~/.depswapper;
-		fi
-
-		if [ ! -e ~/.depswapper/depmapper.json ];then
-    		touch ~/.depswapper/depmapper.json 
-    		echo '{\n\t\"DependencyName\":\"git-url\"\n}' > ~/.depswapper/depmapper.json
-		fi
-		`
-	end
    end
   end
